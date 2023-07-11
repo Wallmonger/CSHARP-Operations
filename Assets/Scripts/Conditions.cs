cfd346d567e09b3d3e64c4549c5993e75c641213
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-11 04:09:43 +0200

    End of conditions

---
+++ Assets/Scripts/Conditions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditions : MonoBehaviour
+{
+    public bool exemple;
+    public Material mat;
+    public int age;
+
+
+    void Start()
+    {
+        if (age > 25)
+        {
+            if (exemple) 
+            {
+                if (1 == 1)
+                {
+                    print("is ok");
+                }
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
