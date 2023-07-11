abe25e5592eb42ec2f9b6bbca05c91a85a4f80bc
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-11 03:50:19 +0200

    variables

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    // Variables
+
+    // Public can be changed in unity itself, Private only in the code
+    public int age = 30;
+    public float argent = 27.956f;
+    public bool exempleBool = true;
+    public string phrase;
+
+    // Monobehaviour brings new variables
+    public Color Couleur;
+    public Material mat;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        age = int.Parse(phrase); // Transform into int
+        mat.color = Couleur;
     }
 
     // Update is called once per frame
