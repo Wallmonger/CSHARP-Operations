87c8d7b071891ec1e6cbff71fcffa383961cdd9a
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-11 05:07:10 +0200

    Array and loop

---
+++ Assets/Scripts/Array.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    public int[] tab;
+    void Start()
+    {
+        tab[2] = 5;
+        /*for(int i =  0; i < tab.Length; i++)
+        {
+            print(tab[i]);
+        }*/
+
+        /*foreach (int val in tab)
+        {
+            print(val);
+        }*/
+
+        print(tab.Length);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
